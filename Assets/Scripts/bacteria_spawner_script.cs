1e6683a388917d5d343d7141fecd871245716edd
Unknown <tomaspsuarez@gmail.com>
2017-12-21 17:31:50 +0100

    Bacteria Spawner Graphics V2
    Made the main animations of the bacteria spawner, added new particles, and the barebones script with variable descriptions

---
+++ Assets/Scripts/bacteria_spawner_script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bacteria_spawner_script : MonoBehaviour {
+
+    private Animator bacteria_animator;
+    private ParticleSystem spawn_fx;
+
+
+	void Start () {
+
+        bacteria_animator = GetComponent<Animator>();
+        spawn_fx = GetComponent<ParticleSystem>();
+
+    }
+
+    void SpawnBacteria() {
+
+        bacteria_animator.SetTrigger("activate");
+
+    }
+
+    void SpawnBacteriaFX() {
+
+        spawn_fx.Play();
+
+    }
+	
+
+}
