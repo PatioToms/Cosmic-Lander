b2e9af7d0e3be8160b1fc39238dc8637f22ca0f1
Tomas Patiño Suarez <tomaspsuarez@gmail.com>
2017-12-30 14:23:32 +0100

    Added the texture pan and scalability for the Toxic cloud texture

---
+++ Assets/Sprites/other sprites/toxic cloud/toxic_cloud_script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toxic_cloud_script: MonoBehaviour {
+
+	[SerializeField] float scrollSpeed_X = 0.5f;
+	[SerializeField] float scrollSpeed_Y = 0.5f;
+	[SerializeField] float ScaleToTiles = 0.667f;
+
+	private Renderer renderTexture;
+		
+	void Awake() {
+	
+		renderTexture = GetComponent<Renderer>();
+
+		float TextureScale_X = transform.lossyScale.x * ScaleToTiles;
+		float TextureScale_Y = transform.lossyScale.z * ScaleToTiles;
+		renderTexture.material.SetTextureScale ("_MainTex", new Vector2 (TextureScale_X, TextureScale_Y));
+	}
+		
+	void Update() {
+			
+		float offset_X = Time.time * scrollSpeed_X;
+		float offset_Y = Time.time * scrollSpeed_Y;
+		renderTexture.material.SetTextureOffset ("_MainTex", new Vector2 (offset_X, offset_Y));
+
+	}
+
+}
