1981134a6be59b7f83b42f9cc944bd0fb358c360
Tomas Patiño Suarez <tomaspsuarez@gmail.com>
2018-01-19 21:10:28 +0100

    Started Implementing the transition to the level intro, and added a script to change scenes more smoothly

---
+++ Assets/Scripts/menu_manager_script.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_manager_script : MonoBehaviour {
+
+	[SerializeField] bool does_it_trigger = false;
+	[SerializeField] float timer = 5f;
+
+	[SerializeField] AudioSource ambient_sound;
+	[SerializeField] Animator fade_animator;
+	[SerializeField] string level_name = "Lvl_MainMenu";
+
+
+	void Start(){
+	
+		if (does_it_trigger) {
+		
+			Invoke ("change_level_generic", timer);
+		
+		}
+	
+	}
+
+
+	public void change_level_special () {
+	
+		fade_animator.SetTrigger ("QuitLevel");
+		StartCoroutine (change_level());
+	
+	}
+
+	public void change_level_generic () {
+
+		StartCoroutine (change_level());
+
+	}
+
+	IEnumerator change_level(){
+
+		while (ambient_sound.volume > 0) {
+		
+			ambient_sound.volume -= Time.fixedDeltaTime * 0.75f;
+			yield return new WaitForFixedUpdate ();
+		
+		}
+			
+		SceneManager.LoadScene(level_name);
+		yield return null;
+	
+	}	
+
+
+}
