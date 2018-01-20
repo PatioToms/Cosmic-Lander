5f51cbbbba1beda9bd902901fc529ac6892375a7
Tomas Patiño Suarez <tomaspsuarez@gmail.com>
2018-01-20 11:47:08 +0100

    Added some sound effects for more impactful texts, and polished the intro screen. Next up is the score screen, and screen flow.

---
+++ Assets/sound_player_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound_player_script : MonoBehaviour {
+
+	[SerializeField] private AudioClip other_clip;
+	[SerializeField] private AudioSource sound_emitter;
+
+	public void playSound(){
+		
+		sound_emitter.pitch = Random.Range (0.7f, 1f);
+		sound_emitter.Play ();
+	
+	}
+
+	public void playSoundSpecial(){
+	
+		sound_emitter.clip = other_clip;
+		sound_emitter.pitch = 1f;
+		sound_emitter.Play ();
+	
+	}
+
+
+}
